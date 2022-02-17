b5d2a9371b705131bc6e75b37028c4dea8646c99
Ismoilov <mobiledeveloper9922@gmail.com>
2022-02-18 00:14:34 +0500

    Initial comment

---
+++ app/src/main/java/com/example/drawingapp/models/PointM.kt
@@ -0,0 +1,3 @@
+package com.example.drawingapp.models
+
+data class PointM(val x: Int, val y: Int, val isDown: Boolean = false)
---
+++ app/src/main/java/com/example/drawingapp/utils/Utils.kt
@@ -0,0 +1,9 @@
+package com.example.drawingapp.utils
+
+import com.example.drawingapp.models.PointM
+
+object Utils {
+
+    val list = mutableListOf<PointM>()
+
+}
